460ea2a07c5701f71113a1fdefdbda1b4a0ef9d6
Наташа <n.e.dolgih@yandex.ru>
2016-05-05 23:42:49 +0500

    more refactor and some additions

--- index.js
+++ index.js
@@ -1,216 +1,253 @@
-var params = {
-    energy: 0,
-    food: 0,
-    happiness: 0
+var Peppa = function (speed) {
+    this.loadData();
+    this.initPeppa(speed);
 };
-var awake = true;
-var speed = 3;
-var recognizer;
-var peppa = document.getElementsByClassName('peppaThePig').item(0);
-var feedBtn = document.getElementsByClassName('feed').item(0);
-var resetBtn = document.getElementsByClassName('reset').item(0);
-var speechLog = document.getElementsByClassName('speechLog').item(0);
-var lifeID;
 
-initPeppa();
-
-function initPeppa() {
-    loadData();
-    setInterval(saveData, 10000);
-    displayData();
-    lifeID = setInterval(lifeController, 1000);
-
-    initSounds();
-    initNotifications();
-    initSpeech();
-    initLightDetection();
-    initFeed();
-
-    document.addEventListener("visibilitychange", handleVisibilityChange);
-    resetBtn.addEventListener('click', reset, false);
-}
-
-function initNotifications() {
-    var Notification = window.Notification ||
-        window.webkitNotification || null;
-    if (Notification) {
-        Notification.requestPermission(
-            function () {
-                setInterval(notify, 60 * 1000);
-            }
-        );
-    }
-}
-
-function initFeed() {
-    if (!navigator.getBattery) {
-        feedBtn.addEventListener('click', feed, false);
-    } else {
-        feedBtn.style.display = 'none';
-    }
-}
-
-function notify() {
-    new Notification('Heyy!', {body: 'Did you forget about me?'});
-}
-
-function reset() {
-    localStorage.removeItem('params');
-    loadData();
-    clearInterval(lifeID);
-    lifeID = setInterval(lifeController, 1000);
-}
-
-function initSpeech() {
-    var SpeechRecognition = window.SpeechRecognition ||
-        window.webkitSpeechRecognition || null;
-    if (SpeechRecognition) {
-        recognizer = new SpeechRecognition;
-        recognizer.lang = 'ru-RU';
-        recognizer.continuous = true;
-        recognizer.interimResults = true;
-        peppa.addEventListener('click', checkTalk);
-        recognizer.addEventListener('result', talkResults)
-    } else {
-        speechLog.style.display = 'none';
-    }
-}
-
-function lifeController() {
-    displayData();
-    hunger();
-    if (awake) {
-        if (params.energy > 0) {
-            params.energy--;
+Peppa.prototype = {
+    foodInc: function() {
+        if (this.awake) {
+            this.food = Math.min(100, this.food + 1 * this.speed);
+            this.stopListening();
+        } else {
+            this.foodDec();
         }
-    } else {
-        if (params.energy < 100) {
-            params.energy = Math.max(100, params.energy + speed);
-        }
-    }
-    params.happiness = Math.max(0, params.happiness - 1);
-    isAlive();
-    
-}
+    },
+    happinessInc: function(value) {
+        this.happiness = Math.min(100, this.happiness + value);
+    },
+    energyInc: function() {
+        this.energy = Math.min(100, this.energy + 1 * this.speed);
+        this.stopListening();
+    },
+    foodDec: function() {
+        this.food = Math.max(0, this.food - 1)
+    },
+    happinessDec: function() {
+        this.happiness = Math.max(0, this.happiness - 1)
+    },
+    energyDec: function() {
+        this.energy = Math.max(0, this.energy - 1)
+    },
 
-function isAlive() {
-    if (params.happiness == 0 && params.food == 0 ||
-    params.happiness == 0 && params.energy == 0 ||
-    params.food == 0 && params.energy == 0) {
-        clearInterval(lifeID);
-        animateDeath();
-    }
-}
-
-function animateDeath() {
-    document.getElementsByClassName();
-}
-
-function hunger() {
-    if (navigator.getBattery) {
-        navigator
-            .getBattery()
-            .then(workBattery);
-        return true;
-    } else {
-        if (params.food > 0) {
-            params.food--;
-        }
-        return false;
-    }
-}
-function workBattery(battery) {
-    if (battery.charging) {
-        if (params.food < 100) {
-            params.food = Math.max(100, params.food + speed);
-        }
-    } else {
-        if (params.food > 0) {
-            params.food--;
-        }
-    }
-}
-
-function feed() {
-    console.log('here');
-    params.food = Math.min(100, params.food + 10);
-}
-
-function checkTalk() {
-    if (params.happiness < 100) {
-        recognizer.start();
-    }
-}
-
-function talkResults(event) {
-    for (var i = event.resultIndex; i < event.results.length; i++) {
-        if (event.results[i].isFinal) {
-            var res = event.results[i][0].transcript;
-            speechLog.innerHTML += res + '<br>';
-            if (params.happiness < 100) {
-                params.happiness = Math.min(100, params.happiness + res.length);
-            }
-        }
-    }
-    if (params.happiness == 100) {
-        recognizer.stop();
-    }
-}
-
-function initSounds() {
-    //TODO
-}
-
-function handleVisibilityChange() {
-    awake = (document.hidden) ? false : true;
-}
-
-
-function initLightDetection() {
-    if ('ondevicelight' in window) {
-        window.addEventListener('devicelight', isDarkOutside);
-    }
-}
-
-function isDarkOutside(e) {
-    awake = e.value > 10;
-}
-
-function saveData() {
-    if (checkLocalStorage()) {
-        localStorage.setItem('params', JSON.stringify(params));
-        console.log('saved');
-    }
-}
-
-function loadData() {
-    if (checkLocalStorage() && hasStoredData()) {
-        params = JSON.parse(localStorage.getItem('params'));
-    } else {
-        params = {
-            energy: 100,
-            food: 100,
-            happiness: 100
+    initPeppa: function (speed) {
+        this.awake = true;
+        this.speed = speed;
+        this.peppa = document.getElementsByClassName('peppaThePig').item(0);
+        this.feedBtn = document.getElementsByClassName('feed').item(0);
+        this.resetBtn = document.getElementsByClassName('reset').item(0);
+        this.speechLog = document.getElementsByClassName('speechLog').item(0);
+        this.volume = document.getElementById('volume');
+        this.sounds = {
+            0: document.getElementById('sound0'),
+            1: document.getElementById('sound1'),
+            2: document.getElementById('sound2'),
+            death: document.getElementById('death')
         };
+        setInterval(() => {this.save();}, 10000);
+        this.displayData();
+        this.lifeID = setInterval(() => {this.lifeController()}, 1000);
+
+        this.initSounds();
+        this.initNotifications();
+        this.initSpeech();
+        this.initLightDetection();
+        this.initFeed();
+
+        document.addEventListener("visibilitychange",  () => {this.handleVisibilityChange()});
+        this.resetBtn.addEventListener('click', (event) => {this.reset(event);}, false);
+    },
+
+    displayData: function () {
+        document.getElementsByClassName('food').item(0).innerText = this.food;
+        document.getElementsByClassName('energy').item(0).innerText = this.energy;
+        document.getElementsByClassName('happiness').item(0).innerText = this.happiness;
+    },
+    save: function () {
+        if (checkLocalStorage()) {
+            localStorage.setItem('peppa', this.toString());
+            console.log('saved');
+        }
+    },
+    toString: function () {
+        var this_ = this;
+        var obj = {
+            food: this_.food,
+            energy: this_.energy,
+            happiness: this_.happiness
+        };
+        return JSON.stringify(obj);
+    },
+    loadData: function () {
+        if (checkLocalStorage() && hasStoredData()) {
+            var res = JSON.parse(localStorage.getItem('peppa'));
+            Object.assign(this, res);
+        } else {
+            this.energy = 100;
+            this.food = 100;
+            this.happiness = 100;
+        }
+    },
+
+    lifeController: function () {
+        this.displayData();
+        this.hunger();
+        if (this.awake) {
+            this.energyDec();
+        } else {
+            this.energyInc()
+        }
+        this.happinessDec();
+        if (!this.isAlive()) {
+            this.die();
+        }
+    },
+    initSounds: function () {
+        this.volume.max = 1;
+        this.volume.step = 0.1;
+        this.volume.defaultValue = 0.5;
+        this.changeVolume();
+        this.volume.addEventListener('change', (event) => {this.changeVolume(event)});
+        setInterval(() => {this.makeSound()}, Math.floor(Math.random() * 15) * 1000);
+    },
+
+    changeVolume: function () {
+        for (var sound in this.sounds) {
+            if (this.sounds.hasOwnProperty(sound)) {
+                this.sounds[sound].volume = this.volume.value;
+            }
+        }
+    },
+    makeSound: function (anyway) {
+        if (this.isAlive() && !this.listening && (!document.hidden || anyway)) {
+            this.sounds[Math.floor(Math.random() * 3)].play();
+        }
+    },
+    initNotifications: function () {
+        var Notification = window.Notification ||
+            window.webkitNotification || null;
+        if (Notification) {
+            Notification.requestPermission()
+                .then((result) => {
+                    if (result === 'denied' || result === 'default') {
+                        console.log('The permission request was dismissed.');
+                        return;
+                    }
+                    setInterval(() => {this.checkStats()}, 5 * 1000)
+                }
+            );
+        }
+    },
+    checkStats: function () {
+        var message = "";
+        if (this.food <= 20) {
+            message += ' I\'m hungry:(';
+        }
+        if (this.happiness <= 20) {
+            message += ' It\'s so boring to be alone:(';
+        }
+        if (message && this.isAlive()) {
+            notify('Hey!', 'Did you forget about me?' + message);
+            this.makeSound(true);
+        }
+    },
+    isAlive: function () {
+        return !(this.happiness == 0 && this.food == 0 ||
+        this.happiness == 0 && this.energy == 0 ||
+        this.food == 0 && this.energy == 0);
+    },
+
+    initSpeech: function () {
+        var SpeechRecognition = window.SpeechRecognition ||
+            window.webkitSpeechRecognition || null;
+        if (SpeechRecognition) {
+            this.recognizer = new SpeechRecognition();
+            this.recognizer.lang = 'ru-RU';
+            this.recognizer.continuous = true;
+            this.recognizer.interimResults = true;
+            this.peppa.addEventListener('click', (event) => {this.checkTalk(event)});
+            this.recognizer.addEventListener('result', (event) => {this.talkResults(event)})
+        } else {
+            this.speechLog.style.display = 'none';
+        }
+    },
+    checkTalk: function () {
+        if (this.happiness < 100) {
+            this.recognizer.start();
+            this.listening = true
+        }
+    },
+    talkResults: function (event) {
+        for (var i = event.resultIndex; i < event.results.length; i++) {
+            if (event.results[i].isFinal) {
+                var res = event.results[i][0].transcript;
+                this.speechLog.innerHTML += res + '<br>';
+                this.happinessInc(res.length);
+            }
+        }
+        if (this.happiness == 100) {
+            this.stopListening();
+        }
+    },
+    stopListening: function () {
+        if (this.listening) {
+            this.listening = false;
+            this.recognizer.stop();
+        }
+    },
+    initLightDetection: function () {
+        if ('ondevicelight' in window) {
+            window.addEventListener('devicelight', event => {this.isDarkOutside(event)});
+        }
+    },
+    isDarkOutside: function (e) {
+        this.awake = e.value > 10;
+    },
+    initFeed: function() {
+        if (!navigator.getBattery) {
+            this.feedBtn.addEventListener('click', event => {this.feed(event)}, false);
+        } else {
+            this.feedBtn.style.display = 'none';
+        }
+    },
+
+    feed: function() {
+        this.foodInc();
+    },
+    hunger: function() {
+        if (navigator.getBattery) {
+            navigator
+                .getBattery()
+                .then((battery) => {this.workBattery(battery)});
+            return true;
+        } else {
+            this.foodDec();
+            return false;
+        }
+    },
+    workBattery: function(battery) {
+        if (battery.charging) {
+            this.foodInc();
+        } else {
+            this.foodDec();
+        }
+    },
+    handleVisibilityChange: function() {
+        this.awake = document.hidden ? false : true;
+    },
+    reset: function () {
+        localStorage.removeItem('peppa');
+        this.loadData();
+        clearInterval(this.lifeID);
+        this.lifeID = setInterval(() => {this.lifeController();}, 1000);
+    },
+    die: function () {
+        clearInterval(this.lifeID);
+        this.animateDeath();
+        this.sounds.death.play()
+    },
+    animateDeath: function () {
+        // TODO
     }
-}
+};
 
-function displayData() {
-    document.getElementsByClassName('food').item(0).innerText = params.food;
-    document.getElementsByClassName('energy').item(0).innerText = params.energy;
-    document.getElementsByClassName('happiness').item(0).innerText = params.happiness;
-}
-
-function hasStoredData() {
-    return !!localStorage.getItem('params');
-}
-
-function checkLocalStorage() {
-    try {
-        localStorage.setItem('key', 'value');
-        localStorage.removeItem('key');
-        return true;
-    } catch (error) {
-        return false;
-    }
-}
+new Peppa(3);
---
+++ lib.js
@@ -0,0 +1,21 @@
+
+
+
+function checkLocalStorage() {
+    try {
+        localStorage.setItem('key', 'value');
+        localStorage.removeItem('key');
+        return true;
+    } catch (error) {
+        return false;
+    }
+}
+
+function hasStoredData() {
+    return !!localStorage.getItem('peppa');
+}
+
+function notify(title, message) {
+    var notif = new Notification(title, {body: message});
+    notif.onclick = () => {window.focus();}
+}
